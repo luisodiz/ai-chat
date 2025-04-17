aa5ca6de9db4c53e5b2471a0f79ec8d0c5e271e1
Alexander Sosov <a.sosov@vlig44.ru>
2025-04-17 17:54:34 +0300

    chore

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
-import { CustomButton } from '@shared/ui/CustomButton';
-import { CustomText } from '@components/ui/CustomText';
+import { CustomButton } from '../src/shared/ui/CustomButton';
+import { CustomText } from '../src/components/ui/CustomText';
 
 export default function HomeScreen() {
   return (
